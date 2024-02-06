8a3d3080e8414c95fa59b430b7193798030f716d
Alalilacias <andresallegue@yahoo.es>
2024-02-06 13:11:31 +0100

    Work done at the academy on the 06/02/2024, from 10:00-14:00h.

---
+++ Backend Java Specialization/S1/T4/n1/exercise1/src/CalendarTest.java
@@ -0,0 +1,8 @@
+package S1.T4.n1.exercise1.src;
+
+import java.util.List;
+
+public class CalendarTest {
+    List<String> calendar = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September",
+            "October", "November", "December");
+}
