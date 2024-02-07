2faae98a182747733277eab63aaa80ba7e0ad90a
Alalilacias <andresallegue@yahoo.es>
2024-02-07 11:10:29 +0100

    Work done at the academy on the 07/02/2024, from 10:00-14:00h.

--- Backend Java Specialization/S1/T4/n1/exercise1/src/Calendar.java
+++ Backend Java Specialization/S1/T4/n1/exercise1/src/Calendar.java
@@ -2,7 +2,8 @@ package S1.T4.n1.exercise1.src;
 
 import java.util.List;
 
-public class CalendarTest {
+public class Calendar {
+//    List filled with months of the calendar, the contents and order of which we'll test
     List<String> calendar = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September",
             "October", "November", "December");
 }
