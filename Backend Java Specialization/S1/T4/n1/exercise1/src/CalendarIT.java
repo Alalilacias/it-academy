e71db057f7d876f90814787d144de15cdcf6c0a0
Alalilacias <andresallegue@yahoo.es>
2024-02-07 11:21:40 +0100

    Work done at the academy on the 07/02/2024, from 10:00-14:00h.

--- Backend Java Specialization/S1/T4/n1/exercise1/src/CalendarIT.java
+++ Backend Java Specialization/S1/T4/n1/exercise1/src/CalendarIT.java
@@ -7,7 +7,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
-public class Calendar {
+public class CalendarIT {
 //    List filled with months of the calendar, the contents and order of which we'll test
     List<String> calendar = List.of("January", "February", "March", "April", "May", "June", "July", "August", "September",
             "October", "November", "December");
