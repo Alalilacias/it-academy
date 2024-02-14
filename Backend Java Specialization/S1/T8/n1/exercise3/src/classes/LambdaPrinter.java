057c1faf9deffb049494dd8d088ae54b21ac5ec4
Alalilacias <andresallegue@yahoo.es>
2024-02-14 17:41:31 +0100

    Work done at home on the 14/02/2024, from 14:00-00:00h.

---
+++ Backend Java Specialization/S1/T8/n1/exercise3/src/classes/LambdaPrinter.java
@@ -0,0 +1,23 @@
+package S1.T8.n1.exercise3.src.classes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdaPrinter {
+    private static final List<String> calendar = Arrays.asList(
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December");
+    protected static void calendarPrinter(){
+        calendar.forEach(calendar -> System.out.println(calendar));
+    }
+}
---
+++ Backend Java Specialization/S1/T8/n1/exercise4/src/classes/Helper.java
@@ -0,0 +1,7 @@
+package S1.T8.n1.exercise4.src.classes;
+
+public class Helper extends LambdaPrinter {
+    protected static void testExercise(){
+        calendarPrinter();
+    }
+}
