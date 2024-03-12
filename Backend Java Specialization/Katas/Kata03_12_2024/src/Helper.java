09c530a0ab2246224c30d1dd0139a3ec9cd10370
Alalilacias <andresallegue@yahoo.es>
2024-03-12 13:21:49 +0100

    Work done at the academy on the 12/03/2024, from 10:00-14:00h.

---
+++ Backend Java Specialization/Katas/Kata03_12_2024/src/Helper.java
@@ -0,0 +1,23 @@
+package Katas.Kata03_12_2024.src;
+
+public class Helper {
+    public static void testKata(String input) {
+        System.out.println(rot13Encrypt(input));
+    }
+    public static String rot13Encrypt(String input) {
+        return input.chars()
+            .map( c -> {
+                if (Character.isLetter(c)){
+                    if       (c >= 'a' && c <= 'm') c += 13;
+                    else if  (c >= 'A' && c <= 'M') c += 13;
+                    else if  (c >= 'n' && c <= 'z') c -= 13;
+                    else if  (c >= 'N' && c <= 'Z') c -= 13;
+                    return (char) (c);
+                }
+                return c;
+            })
+            .mapToObj(c -> (char) c)
+            .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+            .toString();
+    }
+}
