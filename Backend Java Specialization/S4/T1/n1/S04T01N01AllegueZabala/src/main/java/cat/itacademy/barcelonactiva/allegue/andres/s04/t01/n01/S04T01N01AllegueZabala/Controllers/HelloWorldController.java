c7665f54f4fad3aff1aa6636c90b2407556b64c8
Alalilacias <andresallegue@yahoo.es>
2024-04-09 13:50:06 +0200

    Work done at the academy on the 09/04/2024, from 10:00-14:00h.

---
+++ Backend Java Specialization/S4/T1/n1/S04T01N01AllegueZabala/src/main/java/cat/itacademy/barcelonactiva/allegue/andres/s04/t01/n01/S04T01N01AllegueZabala/Controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.allegue.andres.s04.t01.n01.S04T01N01AllegueZabala.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    public String greet(){
+        return "";
+    }
+    public String greet2(){
+        return "";
+    }
+}
