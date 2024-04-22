53d847bea789a84896ae0ff3d9e2db93136f1938
Alalilacias <andresallegue@yahoo.es>
2024-04-22 15:57:43 +0200

    Work done at home on the 22/04/2024, from 14:00-00:00h.

--- Backend Java Specialization/S5/T1/n1/src/main/java/cat/itacademy/barcelonactiva/Allegue/Andres/s05/t01/n01/S05T01N01AllegueAndres/config/OpenApiConfig.java
+++ Backend Java Specialization/S5/T1/n1/src/main/java/cat/itacademy/barcelonactiva/Allegue/Andres/s05/t01/n01/S05T01N01AllegueAndres/config/OpenApiConfig.java
@@ -1,13 +1,24 @@
 package cat.itacademy.barcelonactiva.Allegue.Andres.s05.t01.n01.S05T01N01AllegueAndres.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().components(new Components()).info(
+                new Info()
+                        .title("Dead Island Branch Manager")
+                        .description("Branch manager for clients looking for a more gothic and dark aesthetic when managing their branches.")
+                        .version("1.0"));
+    }
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
