6a4f4abea99db8b11eb2c5de4f6b7e379d8f3753
Alalilacias <andresallegue@yahoo.es>
2024-05-27 23:13:40 +0200

    Work done at the academy on the 27/05/2024, from 10:00-14:00h. Next Day TODOs: - Testing of controllers, services, repositories, exceptions and other classes. - Java Documentation of non-exposed parts of the api. - OpenApi Documentation of exposed parts of the api.

--- Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/controllers/PlayerControllerTest.java
+++ Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/controllers/PlayerControllerTest.java
@@ -1,8 +1,15 @@
 package com.api.fortuna.controllers;
 
 import com.api.fortuna.model.domain.Game;
+import com.api.fortuna.model.domain.Player;
+import com.api.fortuna.model.domain.enums.Roles;
+import com.api.fortuna.model.dto.PlayerDTO;
+import com.api.fortuna.model.dto.responses.GamblingResponse;
+import com.api.fortuna.model.service.FortunaTokenService;
 import com.api.fortuna.model.service.interfaces.PlayerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,14 +19,19 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest()
 @AutoConfigureMockMvc
-@WithMockUser
+@WithMockUser(roles = "USER")
 class PlayerControllerTest {
 
     @Autowired
@@ -28,44 +40,107 @@ class PlayerControllerTest {
     @MockBean
     private PlayerService service;
 
-    private final String token = "valid_token";
+    @MockBean
+    private FortunaTokenService tokenService;
+
+    @InjectMocks
+    private PlayerController controller;
+
+    private String token;
+
+    @BeforeEach
+    public void setUp() {
+        token = "mock_token";
+
+        when(tokenService.generateToken(any(Player.class))).thenReturn(token);
+        when(tokenService.validateToken(anyString(), any(Player.class))).thenReturn(true);
+    }
 
     @Test
     void throwDice() throws Exception {
-        Game game = Mockito.mock(Game.class);
-        when(service.throwDice(token)).thenReturn(game);
+        when(service.throwDice(token)).thenReturn(Mockito.mock(Game.class));
 
-        mockMvc.perform(post("/players/game")
+        mockMvc.perform(post("/players/games")
                         .header("authorization", token)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isOk());
     }
 
     @Test
-    void throwDiceWithAddiction() {
+    void throwDiceWithAddiction() throws Exception {
+        when(service.simulateGamblingAddiction(eq(token), anyDouble())).thenReturn(Mockito.mock(GamblingResponse.class));
+
+        mockMvc.perform(post("/players/games/addiction/0.1")
+                        .header("authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getAllPlayerGames() {
+    void getAllPlayerGames() throws Exception {
+        when(service.getAllPlayerGames(token)).thenReturn(Collections.singletonList(Mockito.mock(Game.class)));
+
+        mockMvc.perform(get("/players/games")
+                        .header("authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getRanking() {
+    void getRanking() throws Exception {
+        when(service.getAll()).thenReturn(Collections.singletonList(Mockito.mock(PlayerDTO.class)));
+
+        mockMvc.perform(get("/players/ranking")
+                        .header("authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getBestRanking() {
+    void getBestRanking() throws Exception {
+        when(service.getAll()).thenReturn(List.of(
+                new PlayerDTO(1L, "Player1", Roles.USER, 0.8F),
+                new PlayerDTO(2L, "Player2", Roles.USER, 0.7F),
+                new PlayerDTO(3L, "Player3", Roles.USER, 0.9F)
+        ));
+
+        mockMvc.perform(get("/players/ranking/winner")
+                        .header("authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void getWorstRanking() {
+    void getWorstRanking() throws Exception {
+        when(service.getAll()).thenReturn(List.of(
+                new PlayerDTO(1L, "Player1", Roles.USER, 0.8F),
+                new PlayerDTO(2L, "Player2", Roles.USER, 0.7F),
+                new PlayerDTO(3L, "Player3", Roles.USER, 0.9F)
+        ));
+
+        mockMvc.perform(get("/players/ranking/loser")
+                        .header("authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void updatePlayer() {
+    void updatePlayer() throws Exception {
+        when(service.update(eq(token), anyString())).thenReturn(Mockito.mock(PlayerDTO.class));
+
+        mockMvc.perform(put("/players")
+                        .header("authorization", token)
+                        .content("Update Username")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    void deletePlayerThrows() {
+    void deletePlayerThrows() throws Exception {
+        when(service.deleteThrows(token)).thenReturn("All games deleted");
+
+        mockMvc.perform(delete("/players/games")
+                        .header("authorization", token))
+                .andExpect(status().isOk());
     }
 }
---
+++ Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/model/dto/responses/ClientAuthResponseTest.java
@@ -0,0 +1,18 @@
+package com.api.fortuna.model.dto.responses;
+
+import com.api.fortuna.model.dto.PlayerDTO;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClientAuthResponseTest {
+
+    @Test
+    void player() {
+    }
+
+    @Test
+    void token() {
+    }
+}
