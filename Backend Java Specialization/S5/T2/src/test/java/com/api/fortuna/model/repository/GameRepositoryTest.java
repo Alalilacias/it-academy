022a38dafa38fc865ad34da66ad14393d687d635
Alalilacias <andresallegue@yahoo.es>
2024-05-28 02:08:31 +0200

    Work done at the academy on the 27/05/2024, from 10:00-14:00h. Next Day TODOs: - Testing of services, repositories, and exceptions. - Java Documentation of non-exposed parts of the api. - OpenApi Documentation of exposed parts of the api.

--- Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/model/repository/GameRepositoryTest.java
+++ Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/model/repository/GameRepositoryTest.java
@@ -1,26 +1,20 @@
 package com.api.fortuna.model.repository;
 
 import com.api.fortuna.model.domain.Game;
-import com.api.fortuna.model.service.interfaces.GameService;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @DataMongoTest
 class GameRepositoryTest {
 
     @Mock
     private GameRepository repository;
-
-    @InjectMocks
-    private GameService service;
 
     List<Game> games = List.of(
             new Game(0),
@@ -32,21 +26,32 @@ class GameRepositoryTest {
 
     @Test
     void deleteAllByPlayerId() {
-        repository.saveAll(games);
-
-        assertEquals(5, repository.findAll().size());
-
         repository.deleteAllByPlayerId(0);
-
-        assertEquals(3, repository.findAll().size());
-
         repository.deleteAllByPlayerId(1);
 
-        assertEquals(1, repository.findAll().size());
+        verify(repository, times(1)).deleteAllByPlayerId(0);
+        verify(repository, times(1)).deleteAllByPlayerId(1);
+        verify(repository, times(0)).deleteAllByPlayerId(5);
     }
 
     @Test
     void findAllByPlayerId() {
+        when(repository.findAllByPlayerId(1))
+                .thenReturn(games.stream()
+                        .filter(game -> game.getPlayerId() == 1)
+                        .toList());
+        assertEquals(2,repository.findAllByPlayerId(1).size());
+
+        when(repository.findAllByPlayerId(0))
+                .thenReturn(games.stream()
+                        .filter(game -> game.getPlayerId() == 0)
+                        .toList());
+        assertEquals(2,repository.findAllByPlayerId(0).size());
+
+        when(repository.findAllByPlayerId(5))
+                .thenReturn(games.stream()
+                        .filter(game -> game.getPlayerId() == 5)
+                        .toList());
         assertEquals(1,repository.findAllByPlayerId(5).size());
     }
 }
--- Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/model/repository/PlayerRepositoryTest.java
+++ Backend Java Specialization/S5/T2/src/test/java/com/api/fortuna/model/repository/PlayerRepositoryTest.java
@@ -1,12 +1,50 @@
 package com.api.fortuna.model.repository;
 
+import com.api.fortuna.model.domain.Player;
+import com.api.fortuna.model.domain.enums.Roles;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class PlayerRepositoryTest {
+
+    @MockBean
+    private PlayerRepository repository;
 
     @Test
     void findPlayerByEmail() {
+        String username = "test username";
+        String email = "test@email.cat";
+        String password = "testPassword";
+
+        when(repository.findPlayerByEmail(email))
+                .thenReturn(Optional.of(Player.builder()
+                                .username(username)
+                                .email(email)
+                                .role(Roles.USER)
+                                .password(password)
+                        .build()));
+
+        Optional<Player> optional = repository.findPlayerByEmail(email);
+
+        assertTrue(optional.isPresent());
+        assertEquals(username, optional.get().getUserPersonalName());
+        assertEquals(email, optional.get().getEmail());
+        assertEquals(password, optional.get().getPassword());
+        assertEquals(Roles.USER, optional.get().getRole());
+        assertEquals(Collections.singleton(new SimpleGrantedAuthority("ROLE_" + Roles.USER.name())).toArray()[0], optional.get().getAuthorities().toArray()[0]);
+        assertEquals(0, optional.get().getDefeats());
+        assertEquals(0, optional.get().getVictories());
+        assertEquals(0, optional.get().getWinRate());
     }
 }
